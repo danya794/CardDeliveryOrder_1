74636422c90bd0d6556c5cd6c13e5d48ffe1c296
Danil Kovalev <dannykvlv@gmail.com>
2023-03-17 11:36:06 +0300

    update

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -14,27 +14,42 @@ public class DataGenerator {
     }
 
     public static String generateDate(int shift) {
+
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
         // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
         // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
-        return date;
+        //return date;
     }
 
     public static String generateCity(String locale) {
+        Faker faker = new Faker();
+        String city = faker.address().cityName();
+        return faker.address().city();
+
         // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
         // с помощью Faker, либо используя массив валидных городов и класс Random
-        return city;
+        //return city;
     }
 
     public static String generateName(String locale) {
+        Faker faker = new Faker();
+        String name = faker.name().fullName();
+        return faker.name().fullName();
+
         // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
         // использовать Faker
-        return name;
+        //return name;
     }
 
     public static String generatePhone(String locale) {
+        Faker faker = new Faker();
+        String phone = faker.phoneNumber().phoneNumber();
+        return faker.phoneNumber().phoneNumber();
+
         // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
         // использовать Faker
-        return phone;
+        //return phone;
     }
 
     public static class Registration {
@@ -42,9 +57,16 @@ public class DataGenerator {
         }
 
         public static UserInfo generateUser(String locale) {
+            Faker faker = new Faker (new Locale(locale));
+
+            String user = faker.name().fullName();
+            String name = faker.name().fullName();
+            String city = faker.address().city();
+            String phone = faker.phoneNumber().phoneNumber();
+
             // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
             // generateName(locale), generatePhone(locale)
-            return user;
+            return new UserInfo(city, name, phone);
         }
     }
 
--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,11 +1,17 @@
 package ru.netology.delivery.test;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
-import static com.codeborne.selenide.Selenide.open;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 class DeliveryTest {
 
@@ -22,6 +28,26 @@ class DeliveryTest {
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(firstMeetingDate);
+        $x("//input[@name='name']").setValue("Иванов Сидоров-Можанский");
+        $(By.cssSelector("[data-test-id='phone'] .input__control")).setValue("+79161234455");
+        $(By.cssSelector("[data-test-id='agreement']")).click();
+        $$("button").find(exactText("Запланировать")).click();
+        $x("//*[contains(@class,'notification__content')]").should(appear, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(text("Встреча успешно забронирована на " + firstMeetingDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE));
+        $("[data-test-id=date] input").setValue(secondMeetingDate);
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        $("[data-test-id='replan-notification'] button").click();
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate));
+
         // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
         // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
         // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
